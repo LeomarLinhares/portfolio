59eff347f49af2a666241530ae1d57304676662f
Leomar Linhares <leomar_linhares@hotmail.com>
2021-03-20 13:59:37 -0300

    Reparo dos links (adicionado o target _blank)

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -4,7 +4,7 @@ export default function JobCard(props) {
     return (
         <div className={styles.jobCard}>
             <a href={`${props.url}`}>
-                <img src={`icons/pages/${props.image}`} alt={`${props.title}`}/>
+                <img src={`icons/pages/${props.image}`} alt={`${props.title}`} target="_blank" />
             <div>
                 <h2>{props.title}</h2>
                 <hr/>
